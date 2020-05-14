276aa0ab7a83a8b462663602aa9693bbf6594c11
Diogo Matheus <dm.matheus@gmail.com>
2020-05-13 21:25:29 -0300

    Refatoração no script da aplicação, pendente inclusão dinâmica de arquivos.

---
+++ application.js
@@ -0,0 +1,325 @@
+// jQuery Validate
+var validator = null;
+$(document).ready(function() {
+	config_form();
+});
+
+/*
+	config_form
+	@desc Config form formatter, dependence and validation.
+*/
+function config_form() {
+    config_field_formatter();
+	config_field_dependence();
+	config_form_validation();
+}
+
+/*
+	config_field_formatter
+	@desc Config fields formatter with Cleave.js.
+*/
+function config_field_formatter() {
+	new Cleave('#telefone_input', { phone: true, phoneRegionCode: 'BR' });
+    new Cleave('#cpf_input', { numericOnly: true, delimiters: ['.', '.', '-'], blocks: [3, 3, 3, 2] });
+    new Cleave('#cep_input', { numericOnly: true, delimiters: ['-'], blocks: [5, 3] });
+    new Cleave('#data_nascimento_input', { numericOnly: true, delimiters: ['/', '/', '/'], blocks: [2, 2, 4] });
+    new Cleave('#sus_input', { numericOnly: true, blocks: [15] });
+    new Cleave('#siape_input', { numericOnly: true, blocks: [15] });
+    new Cleave('#data_sintoma_input', { numericOnly: true, delimiters: ['/', '/', '/'], blocks: [2, 2, 4] });
+    new Cleave('#internacao_dias_input', { numericOnly: true, blocks: [15] });
+    new Cleave('#intubacao_dias_input', { numericOnly: true, blocks: [15] });
+    new Cleave('#hemodialise_dias_input', { numericOnly: true, blocks: [15] });
+    new Cleave('#isolamento_dias_input', { numericOnly: true, blocks: [15] });
+}
+
+/*
+	config_field_dependence
+	@desc Config fields dependence control.
+*/
+function config_field_dependence() {
+	toggle_target_field('diagnostico_formal', 'Sim', 'diagnostico_unidade_input');
+	toggle_target_field('historico_internacao', 'Sim', 'internacao_dias_input');
+	toggle_target_field('historico_intubacao', 'Sim', 'intubacao_dias_input');
+	toggle_target_field('historico_hemodialise', 'Sim', 'hemodialise_dias_input');
+	toggle_target_field('necessidade_especifica', 'Sim', 'necessidade_descricao_input');
+	toggle_target_field('isolamento_social', 'Sim', 'isolamento_dias_input');
+	toggle_target_field('sintomas[]', 'Outros', 'outros_sintomas_input', true);
+}
+
+/*
+	config_form_validation
+	@desc Config form validation with jQuery validate.
+*/
+function config_form_validation() {
+	$.validator.setDefaults({ ignore: [] });
+	validator = $('#covid-19-diagnosis-form').validate({
+		rules: {
+			// Identificação
+			'nome': { 'required': true, 'maxlength': 100 },
+			'sexo': 'required',
+			'email': { 'email': true },
+			'nacionalidade': 'required',
+			'cpf': 'required',
+			'data_nascimento': 'required',
+			'sus': { 'digits': true },
+			'siape': { 'digits': true },
+			'nome_mae': { 'required': true, 'maxlength': 100 },
+			'raca': 'required',
+			'telefone': 'required',
+			'pais_residencia': 'required',
+			'cep': 'required',
+			'endereco': { 'required': true, 'maxlength': 255 },
+			'ocupacao': { 'required': true, 'maxlength': 100 },
+			'ocupacao_unidade': { 'maxlength': 100 },
+			// Dados clínicos
+			'data_sintoma': 'required',
+			'gestante_amamentando': 'required',
+			'sintomas[]': 'required',
+			// Dados de exposição e viagens
+			'trabalhando_atualmente': 'required',
+			'linha_frente': 'required',
+			'forma_trabalho': 'required',
+			'viagem_recente': 'required',
+			'quantidade_moradores': 'required',
+			'proximidade_idoso_gestante': 'required',
+			'proximidade_crianca': 'required',
+			'proximidade_caso_suspeito': 'required',
+			'proximidade_caso_confirmado': 'required',
+			'convive_animal_externo': 'required',
+			'proximidade_animais': 'required',
+			'carrinho_feira': 'required',
+			'carro_particular': 'required',
+			'estacionamento_publico_coletivo': 'required',
+			// Atendimento especializado
+			'diagnostico_formal': 'required',
+			'diagnostico_unidade': { 'maxlength': 100 },
+			'historico_internacao': 'required',
+			'internacao_dias': { 'digits': true },
+			'historico_intubacao': 'required',
+			'intubacao_dias': { 'digits': true },
+			'historico_hemodialise': 'required',
+			'hemodialise_dias': { 'digits': true },
+			'historico_testagem': 'required',
+			'necessidade_especifica': 'required',
+			'isolamento_social': 'required',
+			'isolamento_dias': { 'digits': true },
+			'estado_atual': 'required'
+      		// 'xpto': { 'extension': 'jpeg|jpg|png|pdf' }
+		},
+		messages: {
+			// Identificação
+			'nome': {
+				'required': 'Identificação: Por favor, informe seu nome.',
+				'maxlength': 'Identificação: Seu nome deve conter no máximo 100 caracteres.'
+			},
+			'sexo': 'Identificação: Por favor, informe seu sexo.',
+			'email': {
+				'email': 'Identificação: Por favor, informe um email válido.'
+			},
+			'nacionalidade': 'Identificação: Por favor, informe sua nacionalidade.',
+			'cpf': 'Identificação: Por favor, informe seu cpf.',
+			'data_nascimento': 'Identificação: Por favor, informe sua data de nascimento.',
+			'sus': {
+				'digits': 'Atendimento especializado: Por favor, use apenas dígitos ao informar o número do cartão SUS/CNS.'
+			},
+			'siape': {
+				'digits': 'Atendimento especializado: Por favor, use apenas dígitos ao informar o SIAPE.'
+			},
+			'nome_mae': {
+				'required': 'Identificação: Por favor, informe o nome da sua mãe.',
+				'maxlength': 'Identificação: O nome da sua mãe deve conter no máximo 100 caracteres.'
+			},
+			'raca': 'Identificação: Por favor, informe sua raça.',
+			'telefone': 'Identificação: Por favor, informe um telefone de contato.',
+			'pais_residencia': 'Identificação: Por favor, informe seu país de residência.',
+			'cep': 'Identificação: Por favor, informe seu CEP.',
+			'endereco': {
+				'required': 'Identificação: Por favor, informe seu endereço.',
+				'maxlength': 'Identificação: Seu endereço deve conter no máximo 255 caracteres.'
+			},
+			'ocupacao': {
+				'required': 'Identificação: Por favor, informe sua ocupação/cargo.',
+				'maxlength': 'Identificação: Sua ocupação/cargo deve conter no máximo 100 caracteres.'
+			},
+			'ocupacao_unidade': {
+				'maxlength': 'Identificação: Sua unidade/departamento deve conter no máximo 100 caracteres.'
+			},
+			// Dados clínicos
+			'data_sintoma': 'Dados clínicos: Por favor, informe a data dos primeiros sintomas.',
+			'gestante_amamentando': 'Dados clínicos: Por favor, informe se você está grávida ou amamentando.',
+			'sintomas[]': 'Dados clínicos: Por favor, informe os sintomas apresentados.',
+			// Dados de exposição e viagens
+			'trabalhando_atualmente': 'Dados de exposição e viagens: Por favor, informe se você está trabalhando atualmente.',
+			'linha_frente': 'Dados de exposição e viagens: Por favor, informe se você trabalha na linha de frente.',
+			'forma_trabalho': 'Dados de exposição e viagens: Por favor, informe de que forma você está realizando suas funções de trabalho.',
+			'viagem_recente': 'Dados de exposição e viagens: Por favor, informe sobre seu possível histórico de viagem.',
+			'quantidade_moradores': 'Dados de exposição e viagens: Por favor, informe quantas pessoas moram na sua residência.',
+			'proximidade_idoso_gestante': 'Dados de exposição e viagens: Por favor, informe se você convive ou manteve contato com idosos ou gestantes.',
+			'proximidade_crianca': 'Dados de exposição e viagens: Por favor, informe se você convive ou manteve contato com crianças.',
+			'proximidade_caso_suspeito': 'Dados de exposição e viagens: Por favor, informe sobre seu possível contato com casos suspeitos.',
+			'proximidade_caso_confirmado': 'Dados de exposição e viagens: Por favor, informe sobre seu possível contato com casos confirmados.',
+			'convive_animal_externo': 'Dados de exposição e viagens: Por favor, informe sobre seu possível convivio com animal de estimação que passeia na rua.',
+			'proximidade_animais': 'Dados de exposição e viagens: Por favor, informe sobre seu possível contato com animais em áreas afetadas.',
+			'carrinho_feira': 'Dados de exposição e viagens: Por favor, informe se você faz uso de carrinho de feira ou sacola com rodinhas.',
+			'carro_particular': 'Dados de exposição e viagens: Por favor, informe se você utiliza carro particular.',
+			'estacionamento_publico_coletivo': 'Dados de exposição e viagens: Por favor, informe se você utiliza estacionamento público ou coletivo.',
+			// Atendimento especializado
+			'diagnostico_formal': 'Atendimento especializado: Por favor, informe se você já foi diagnosticado formalmente.',
+			'diagnostico_unidade': {
+				'maxlength': 'Atendimento especializado: Unidade de saúde do atendimento deve conter no máximo 100 caracteres.'
+			},
+			'historico_internacao': 'Atendimento especializado: Por favor, informe se vocẽ ficou internado.',
+			'internacao_dias': {
+				'digits': 'Atendimento especializado: Por favor, use apenas dígitos ao informar os dias de internação.'
+			},
+			'historico_intubacao': 'Atendimento especializado: Por favor, informe se vocẽ foi intubado.',
+			'intubacao_dias': {
+				'digits': 'Atendimento especializado: Por favor, use apenas dígitos ao informar os dias de intubação.'
+			},
+			'historico_hemodialise': 'Atendimento especializado: Por favor, informe se vocẽ fez hemodiálise.',
+			'hemodialise_dias': {
+				'digits': 'Atendimento especializado: Por favor, use apenas dígitos ao informar os dias de hemodiálise.'
+			},
+			'historico_testagem': 'Atendimento especializado: Por favor, informe se vocẽ fez alguma testagem para o novo coronavírus (COVID-19).',
+			'necessidade_especifica': 'Atendimento especializado: Por favor, informe se alguma necessidade específica foi identificada no seu atendimento.',
+			'isolamento_social': 'Atendimento especializado: Por favor, informe se vocẽ está em isolamento/distânciamento social.',
+			'isolamento_dias': {
+				'digits': 'Atendimento especializado: Por favor, use apenas dígitos ao informar os dias de isolamento.'
+			},
+			'estado_atual': 'Atendimento especializado: Por favor, informe seu estado atual.'
+      		/*
+      		'xpto': {
+				'extension': 'Atendimento especializado: Extensão do exame digitalizado inválida, selecione um documento válido (jpeg|jpg|png|pdf).'
+			}
+			*/
+		},
+		errorContainer: $('#error-container'),
+		errorLabelContainer: $('ul', $('#error-container')),
+		wrapper: 'li',
+		errorClass: 'invalid',
+		validClass: 'valid',
+		focusInvalid: false,
+		highlight: function(element, errorClass, validClass) {
+			if (
+				element.tagName == 'INPUT' &&
+				($(element).attr('type') == 'radio' || $(element).attr('type') == 'checkbox')
+			)
+				$('span[for="' + element.name + '"]').removeClass(validClass).addClass(errorClass);
+
+			$(element).removeClass(validClass).addClass(errorClass);
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			if (
+				element.tagName == 'INPUT' &&
+				($(element).attr('type') == 'radio' || $(element).attr('type') == 'checkbox')
+			)
+				$('span[for="' + element.name + '"]').removeClass(errorClass).addClass(validClass);
+
+			$(element).removeClass(errorClass).addClass(validClass);
+		},
+		invalidHandler: function(form, validator) {
+            var errors = validator.numberOfInvalids();
+            if (errors) $('html, body').animate({ scrollTop: 0 }, 'fast');
+        },
+        submitHandler: function(form) {
+        	display_result();
+        }
+	});
+
+	// Form checkbox force validation
+	$('.validate-checkbox').on('change', function () {
+		if(typeof validator !== 'undefined' && validator !== null) $(this).valid();
+	});
+
+	// Form reset configuration
+	$('#covid-19-diagnosis-form-reset').on('click', function(e) {
+		$('input').prop('value', '');
+		$('input[type="checkbox"]:checked, input[type="radio"]:checked').prop('checked', false);
+		if(typeof validator !== 'undefined' && validator !== null)
+			validator.destroy();
+
+		config_form_validation();
+	});
+}
+
+/*
+	display_result
+	@desc Display the analysis result.
+*/
+function display_result() {
+	var icon_label = null, title_color = null, message = null;
+	var birthdate = $('#data_nascimento_input').val();
+	var symptoms = get_symptoms();
+	switch(analyze_data(symptoms, birthdate)) {
+		case 'SG':
+			icon_label = 'warning';
+			title_color = 'orange';
+			message = 'Com base nos dados fornecidos, existe uma suspeita de COVID-19 (coronavírus) relacionada com os sintomas de Síndorme Gripal (SG). Considere realizar uma consulta no hospital mais próximo, principalmente, em caso de evolução dos sintomas.';
+			break;
+		case 'SRAG':
+			icon_label = 'warning';
+			title_color = 'orange';
+			message = 'Com base nos dados fornecidos, existe uma suspeita de COVID-19 (coronavírus) relacionada com os sintomas de Síndrome Respiratória Aguda Grave (SRAG). Por favor, dirija-se ao hospital mais próximo.';
+			break;
+		default:
+			icon_label = 'check';
+			title_color = 'green';
+			message = 'Com base nos dados fornecidos, não há suspeita de de COVID-19 (coronavírus). Continue seguindo as instruções do ministério da saúde.';
+			break;
+	}
+	
+	var username = $('#nome_input').val();
+	var icon_element = $('<i class="material-icons medium"></i>').text(icon_label);
+	var title_element = $('<h5 class="valign-wrapper"></h5>')
+	title_element.css('color', title_color);
+	title_element.append(icon_element)
+				 .append(' Olá ' + username);
+
+    var message_element = $('<p></p>').text(message);
+    var back_element = $('<p class="center"><a href="index.html">Voltar ao formulário</a></p>');
+
+    var content = $('<div class="card-panel z-depth-3"></div>');
+    content.append(title_element)
+    	   .append(message_element)
+    	   .append(back_element);
+
+	$('#main-content').html('<br />').append(content);
+	$('html, body').animate({ scrollTop: 0 }, 'fast');
+}
+
+/*
+	toggle_target_field
+	@desc Toggle target input based on expected source value.
+*/
+function toggle_target_field(source, expected, target, isArray = false) {
+	$('input[name="' + source + '"]').on('change', function () {
+		var disable_element = null;
+		if (isArray) {
+			var values = [];
+			$.each($('input[name="' + source + '"]:checked'), function() { values.push($(this).val()); });
+			disable_element = !values.includes(expected);
+		} else {
+			disable_element = !($(this).val() === expected);
+		}
+
+		var target_element = $('#' + target);
+		if (disable_element) {
+			if(!target_element.attr('disabled')) {
+				target_element.val('');
+				target_element.attr('disabled', 'disabled');
+			}
+		} else {
+			target_element.removeAttr('disabled');
+		}
+	});
+}
+
+/*
+	get_symptoms
+	@desc Get the checked symptoms.
+*/
+function get_symptoms() {
+	var symptoms = [];
+	$.each($('input[name="sintomas[]"]:checked'), function() { symptoms.push($(this).val()); });
+	return symptoms;
+}
